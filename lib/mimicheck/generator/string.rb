fcc22be6d4da1fa5e031c94d250560c981621bc8
renan-ranelli <renanranelli@gmail.com>
2015-07-09 13:32:38 -0300

    Fix bug in string generator

--- lib/mimicheck/generator/string.rb
+++ lib/mimicheck/generator/string.rb
@@ -3,14 +3,14 @@ module MimiCheck
     class String
       include Generator
 
-      def initialize(range = 25)
+      def initialize(range = 0..25)
         @range = range
       end
       attr_reader :range
       private :range
 
       def generate
-        Array.new(Char.new, range).join('')
+        Array.new(Char.new, range).call.join('')
       end
     end
   end
